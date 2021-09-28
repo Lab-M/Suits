fc7c62527b16750440fd791c35a5beef689a288b
Mustaqeem Quadry <Mustaqeem@relianceinfosystems.com>
2021-09-28 03:40:44 -0400

    Adding the kitchen codebase

---
+++ Kitchen/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Kitchen.Pages.Restaurants
+{
+    public class ListModel : PageModel
+    {
+        public string Message { get; set; }
+        public void OnGet()
+        {
+            Message = "Hello, baba for the Ladies";
+        }
+    }
+}
